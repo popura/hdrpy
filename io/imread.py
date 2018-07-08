d1ef29323e2d1ef3b1e48952760aadd4b837572f
popura <popura.net@gmail.com>
2018-07-08 18:41:54 +0900

    fixed ExrReader

--- io/imread.py
+++ io/imread.py
@@ -4,14 +4,24 @@ import OpenEXR
 import Imath
 import numpy as np
 from pathlib import Path
+import cv2
+from colour import RGB_COLOURSPACES
+
 
 def imread(path):
     path = Path(path)
     ext = path.suffix
     reader = ImReader.get(ext)
-    image = reader.imread(str(path))
+    try:
+        image = reader.imread(str(path))
+    except TypeError as e:
+        print("at {0}".format(path.name))
+        print(e)
+    print(path.name)
+    print(image.shape)
 
     return image
+
 
 class ImReader():
     @staticmethod
@@ -20,7 +30,7 @@ class ImReader():
             reader = ImReader.generate_reader(ext)
         except TypeError as e:
             print(e)
-        
+
         return reader
 
     @staticmethod
@@ -36,36 +46,96 @@ class ImReader():
 
         return reader
 
+
 class ExrReader():
     def __init__(self, ):
+        self.pt = Imath.PixelType(Imath.PixelType.FLOAT)
         return
 
     def imread(self, path):
-        pt = Imath.PixelType(Imath.PixelType.FLOAT)
         exrfile = OpenEXR.InputFile(path)
+        channel_type = set(exrfile.header()["channels"].keys())
+        if channel_type >= {"R", "G", "B"}:
+            image = self.read_rgb_channels(exrfile)
+        elif channel_type >= {"Y", "BY", "RY"}:
+            image = self.read_ycbcr_channels(exrfile)
+        elif channel_type >= {"Y"}:
+            image = self.read_y_channel(exrfile)
+        else:
+            raise TypeError("This channel type is not supported.")
 
+        exrfile.close()
+        return image
+
+    def read_rgb_channels(self, exrfile):
         dw = exrfile.header()["dataWindow"]
         size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
+        (redstr, greenstr, bluestr) = exrfile.channels("RGB", self.pt)
+        red = np.fromstring(redstr, dtype=np.float32)
+        green = np.fromstring(greenstr, dtype=np.float32)
+        blue = np.fromstring(bluestr, dtype=np.float32)
 
-        (redstr, greenstr, bluestr) = exrfile.channels("RGB", pt)
-        red = np.fromstring(redstr, dtype = np.float32)
-        green = np.fromstring(greenstr, dtype = np.float32)
-        blue = np.fromstring(bluestr, dtype = np.float32)
-
-        image = np.zeros((size[1], size[0], 3), dtype = np.float)
+        image = np.zeros((size[1], size[0], 3), dtype=np.float)
         image[:, :, 0] = red.reshape(size[1], size[0])
         image[:, :, 1] = green.reshape(size[1], size[0])
         image[:, :, 2] = blue.reshape(size[1], size[0])
-        exrfile.close()
+        return image
+
+    def read_ycbcr_channels(self, exrfile):
+        channel = exrfile.header()["channels"]
+        dw = exrfile.header()["dataWindow"]
+        size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
+
+        image = np.zeros((size[1], size[0], 3), dtype=np.float)
+        for i, ch in enumerate(["Y", "BY", "RY"]):
+            chstr = exrfile.channel(ch, self.pt)
+            charray = np.fromstring(chstr, dtype=np.float32)
+            x_subsample = channel[ch].xSampling
+            y_subsample = channel[ch].ySampling
+            charray.shape = (size[1]//y_subsample, size[0]//x_subsample)
+            image[:, :, i] = cv2.resize(charray, None,
+                                        fx=x_subsample, fy=y_subsample,
+                                        interpolation=cv2.INTER_LINEAR)
+        image = self.ycbcr_to_rgb(image)
 
         return image
+
+    def read_y_channel(self, exrfile):
+        dw = exrfile.header()["dataWindow"]
+        size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
+        chstr = exrfile.channel("Y", self.pt)
+        charray = np.fromstring(chstr, dtype=np.float32)
+        image = np.zeros((size[1], size[0], 3), dtype=np.float)
+        image[:, :, 0] = charray.reshape(size[1], size[0])
+        image[:, :, 1] = charray.reshape(size[1], size[0])
+        image[:, :, 2] = charray.reshape(size[1], size[0])
+        
+        return image
+
+
+    def rgb_to_ycbcr(self, rgb):
+        pass
+
+    def ycbcr_to_rgb(self, ycbcr):
+        colourspace = RGB_COLOURSPACES["ITU-R BT.709"]
+        y_weight = colourspace.RGB_to_XYZ_matrix[1, :]
+        rgb = np.zeros(ycbcr.shape, dtype=float)
+        rgb[:, :, 0] = ycbcr[:, :, 2] * ycbcr[:, :, 0] + ycbcr[:, :, 0]
+        rgb[:, :, 2] = ycbcr[:, :, 1] * ycbcr[:, :, 0] + ycbcr[:, :, 0]
+        rgb[:, :, 1] = (ycbcr[:, :, 0]
+                        - y_weight[0] * rgb[:, :, 0]
+                        - y_weight[2] * rgb[:, :, 2])
+        rgb[:, :, 1] /= y_weight[1]
+
+        return rgb
+
 
 class HdrReader():
     def __init__(self, ):
         self.HDR_NONE = 0x00
         self.HDR_RLE_RGBE_32 = 0x01
         return
-    
+
     def imread(self, path):
         with open(path, "rb") as im_file:
             bufsize = 4096
@@ -176,7 +246,7 @@ class HdrReader():
             raise Exception('Failed to load file "{0}"'.format(path))
 
         return img
-        
+
 class PfmReader():
     def __init__(self, ):
         return
