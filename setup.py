5725069f63b6f799e0b226da3941172a84db8c5c
popura <popura.net@gmail.com>
2019-01-31 16:51:16 +0900

    some fixed

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name='hdrpy',
+    version='0.0.2',
+    description='A package for handling high dynamic range images',
+    long_description=readme,
+    author='Yuma Kinoshita',
+    url='https://github.com/popura/hdrpy',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=['numpy', 'scipy', 'opencv-python',
+                      'OpenEXR', 'colour-science', 'colour-hdri']
+)
